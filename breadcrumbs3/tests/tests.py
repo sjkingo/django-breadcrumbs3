d57765c59583f23b4ea2e1959928efd1772dfcc5
Sam Kingston <sam@sjkwi.com.au>
2015-11-08 18:35:39 +1000

    Fixed travis build 13

--- breadcrumbs3/tests/tests.py
+++ breadcrumbs3/tests/tests.py
@@ -23,28 +23,28 @@ TEMPLATES_DJANGO_18 = [
 )
 class BreadcrumbsTest(TestCase):
 
-    @override_settings(BREADCRUMBS_ADD_HOME=False)
+    @override_settings(BREADCRUMBS_HOME_LINK=False)
     def test_breadcrumbs_no_url(self):
         response = self.client.get(reverse('test_no_url'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,
             '<ul class="breadcrumbs-list"><li class="breadcrumb">Some title</li></ul>')
 
-    @override_settings(BREADCRUMBS_ADD_HOME=False)
+    @override_settings(BREADCRUMBS_HOME_LINK=False)
     def test_breadcrumbs_with_url(self):
         response = self.client.get(reverse('test_with_url'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,
             '<ul class="breadcrumbs-list"><li class="breadcrumb"><a href="/theres-just-no-stopping-in-a-white-zone/">Some other title</a></li></ul>')
 
-    @override_settings(BREADCRUMBS_ADD_HOME=True)
+    @override_settings(BREADCRUMBS_HOME_LINK=True)
     def test_breadcrumbs_no_url_with_home(self):
         response = self.client.get(reverse('test_no_url'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response,
             '<ul class="breadcrumbs-list"><li class="breadcrumb"><a href="/">Home</a></li><li class="breadcrumb">Some title</li></ul>')
 
-    @override_settings(BREADCRUMBS_ADD_HOME=True)
+    @override_settings(BREADCRUMBS_HOME_LINK=True)
     def test_breadcrumbs_with_url_with_home(self):
         response = self.client.get(reverse('test_with_url'))
         self.assertEqual(response.status_code, 200)
