c12b58be7ace02fe78bfe86d205ad9fc4e6549ad
Sam Kingston <sam@sjkwi.com.au>
2015-11-08 13:16:35 +1000

    Add classifiers for pypi

--- setup.py
+++ setup.py
@@ -14,4 +14,18 @@ setup(
         'Django',
     ],
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
